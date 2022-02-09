200f17377d0e6aa1202e3b9bbfbb3cc75a230e3f
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-02-09 10:11:38 -0300

    Start repo

---
+++ src/dictionaries.py
@@ -0,0 +1,24 @@
+series_dict = {
+    "United States Mint&#039;s &quot;America the Beautiful&quot; Quarters® Program": "Q463177"
+}
+
+
+composition_dict = {
+    "Bronze": "Q34095",
+    "Copper-nickel": "Q649090",
+    "Copper-nickel clad copper": "Q753",
+    "Silver (.900)": "Q110861802",
+    "Stainless steel": "Q172587",
+    "Nickel": "Q744",
+}
+
+issuer_dict = {"Brazil": "Q155", "Malta": "Q233", "United States": "Q30"}
+
+currency_dict = {
+    "Real (1799-1942)": "Q10366257",
+    "Real (1994-date)": "Q173117",
+    "Cruzeiro (1990-1993)": "Q10261896",
+    "Lira (1972-2007)": "Q174467",
+    "Dollar (1785-date)": "Q4917",
+    "Cruzeiro novo  (1967-1986)": "Q10261894",
+}
